022a168a927c2708467c6d5442e54a23b55955f0
Marius Nita <m@mariusnita.com>
2014-01-01 13:40:50 -0700

    cleanup; improve comp25;

--- Composition25.js
+++ Composition25.js
@@ -5,21 +5,17 @@ function Composition25(ctx) {
 Composition25.prototype = clone(Composition.prototype);
 
 Composition25.prototype.runFunc = function() {
-
-    //console.log(this.x, this.y);
-        var rx = Math.random()*20;
-        var ry = Math.random()*20;
+    var rx = Math.random()*20;
+    var ry = Math.random()*20;
 
     if (Math.random() > 0.5) { rx = -rx; }
     if (Math.random() > 0.5) { ry = -ry; }
-
-    //console.log(this.flip_x, this.flip_y);
     
     if (!this.flip_x) {
         this.momentum_x += rx;
-        } else {
+    } else {
         this.momentum_x -= rx;
-        }
+    }
 
     if (!this.flip_y) {
         this.momentum_y += ry;
@@ -29,27 +25,25 @@ Composition25.prototype.runFunc = function() {
 
     this.drawLine(this.x,this.y,this.x + this.momentum_x,this.y + this.momentum_y,this.color,3);
 
-        this.x += this.momentum_x;
-        this.y += this.momentum_y;
+    this.x += this.momentum_x;
+    this.y += this.momentum_y;
 
-    //this.fillCircle(this.x,this.y,5,'black');
-
-    if (/* this.x < 0 || this.x > this.width || this.y < 0 || this.y > this.height*/ Math.random() > 0.8) {
+    if (Math.random() > 0.8) {
         this.init();
     }
 
-    if (Math.random() > 0.9999) {
-        this.blank('rgba(255,255,255,0.1)');
+    if (Math.random() > 0.99) {
+        this.blank('rgba(255,255,255,0.01)');
     }
 
     this.setTimeout(this.objectExpr() + '.runFunc()');
 };
 
 Composition25.prototype.init = function() {
-        this.x = Math.random() * this.width;
-        this.y = Math.random() * this.height;
-        this.momentum_x = 1;
-        this.momentum_y = 1;
+    this.x = Math.random() * this.width;
+    this.y = Math.random() * this.height;
+    this.momentum_x = 1;
+    this.momentum_y = 1;
     this.color = 'black';
 };
 
