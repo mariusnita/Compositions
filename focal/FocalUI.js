7e0aa29cdd424a479565179355824d336b26fcce
Marius Nita <m@mariusnita.com>
2014-01-06 12:53:54 -0700

    improve code structure; add "no menu" option

--- focal/FocalUI.js
+++ focal/FocalUI.js
@@ -1,128 +1,142 @@
-var anim = null;
-var opts = {menu: true};
+var FocalUI = (function() {
+    var anim = null;
+    var started = false;
+    var opts = {
+        nomenu: false
+    };
+    var menuShown = true;
 
-var names = {
-    play : ['play','pause'],
-    distrib : ['spread','focal'],
-    cols : ['mono','colors'],
-    flyToTheLight : ['attract','radiate'],
-    bound : ['bound','unbound'],
-    mouse : ['mouse','unmouse']
-};
+    var names = {
+        play : ['play','pause'],
+        distrib : ['spread','focal'],
+        cols : ['mono','colors'],
+        flyToTheLight : ['attract','radiate'],
+        bound : ['bound','unbound'],
+        mouse : ['mouse','unmouse'],
+        nomenu : ['no menu', 'menu']
+    };
 
-// yet unused
-function menuShow() {
-    $('.hid').show();
-    $('.first').css('border-radius', '5px 5px 0px 0px');
-}
-
-function menuHide() {
-    setTimeout(function() { 
-        if (!opts.menu) { 
-            $('.hid').hide();
-            $('.first').css('border-radius', '5px');
-        } 
-    }, 200);
-}
-
-
-$(document).ready(function() {
-    var canvas = document.getElementById("focalCanvas");
-
-    canvas.width = $(window).width();
-    canvas.height = $(window).height();
-
-    anim = new Focal(canvas);
-    anim.init();
-
-    for (var k in names) {
-        $('#' + k).html(names[k][0]);
+    function menuShowRaw() {
+        $('.hid').show();
+        $('.first').css('border-radius', '5px 5px 0px 0px');
     }
 
-    // $(document).on('mouseenter', '.first,.hid', menuShow);
-    // $(document).on('mouseleave', '.first,.hid', menuHide);
-    
-    $(document).on('keydown',document,function(evt) {
-        if (evt.keyCode === 83) { // p
-            startStop();
-        }
-        if (evt.keyCode === 82) { // r
-            restart();
-        }
-        if (evt.keyCode === 67) { // c
-            cols();
-        }
-        if (evt.keyCode === 88) { // x
-            bound();
-        }
-        if (evt.keyCode === 65) { // a
-            flyToTheLight();
-        }
-        if (evt.keyCode === 77) { // m
-            mouse();
-        }
-        if (evt.keyCode === 78) { // m
-            menu();
-        }
-    });
-    restart();
-});
-
-var started = false;
-
-function startStop() {
-    if (started) { 
-        anim.stop(); 
-    }
-    else { 
-        anim.run(); 
+    function menuHideRaw() {
+        $('.hid').hide();
+        $('.first').css('border-radius', '5px');
     }
 
-    started = !started;
+    function menuShow() {
+        if (!opts.nomenu)
+            return;
+        menuShown = true;
+        menuShowRaw();
+    }
 
-    $('#play').html(names.play[0 + started]);
-}
+    function menuHide() {
+        if (!opts.nomenu)
+            return;
+        menuShown = false;
+        setTimeout(function() { 
+            if (!menuShown) { 
+                menuHideRaw();
+            } 
+        }, 500);
+    }
 
-function doOpt(prop) {
-    var dist = (opts[prop] === true);
+    function startStop() {
+        if (started) { 
+            anim.stop(); 
+        }
+        else { 
+            anim.run(); 
+        }
 
-    opts[prop] = !opts[prop];
-    $('#' + prop).html(names[prop][0+opts[prop]]);
+        started = !started;
 
-    opts.menu ? menuShow() : menuHide();
+        $('#play').html(names.play[0 + started]);
+    }
 
-    anim.setOpts(opts);
-}
+    function doOpt(prop) {
+        opts[prop] = !opts[prop];
+        $('#' + prop).html(names[prop][0+opts[prop]]);
 
-function restart() {
-    var type = $('input[name=restart_type]:checked').val();
+        anim.setOpts(opts);
+    }
 
-    started = false;
-    anim.stop();
-    opts.distrib = type === 'cover';
-    opts.cols = true;
-    anim.setOpts(opts);
-    anim.init();
-    startStop();
-}
+    function restart() {
+        var type = $('input[name=restart_type]:checked').val();
 
-function cols() {
-    doOpt('cols');
-}
+        started = false;
+        anim.stop();
+        opts.distrib = type === 'cover';
+        opts.cols = true;
+        anim.setOpts(opts);
+        anim.init();
+        startStop();
+    }
 
-function bound() {
-    doOpt('bound');
-}
+    function cols() {
+        doOpt('cols');
+    }
 
-function flyToTheLight() {
-    doOpt('flyToTheLight');
-}
+    function bound() {
+        doOpt('bound');
+    }
 
-function mouse() {
-    doOpt('mouse');
-}
+    function flyToTheLight() {
+        doOpt('flyToTheLight');
+    }
 
-// unused
-function menu() {
-    doOpt('menu');
-}
+    function mouse() {
+        doOpt('mouse');
+    }
+
+    function nomenu() {
+        doOpt('nomenu');
+        opts.nomenu ? menuHideRaw() : menuShowRaw();
+    }
+
+    function initialize() {
+        var canvas = document.getElementById("focalCanvas");
+
+        canvas.width = $(window).width();
+        canvas.height = $(window).height();
+
+        anim = new Focal(canvas);
+        anim.init();
+
+        for (var k in names) {
+            $('#' + k).html(names[k][0]);
+        }
+
+        $(document).on('mouseenter', '.first,.hid', menuShow);
+        $(document).on('mouseleave', '.first,.hid', menuHide);
+        
+        $(document).on('keydown',document,function(evt) {
+            switch (evt.keyCode) {
+                case 83: return startStop(); // p
+                case 82: return restart(); // r
+                case 67: return cols(); // c
+                case 88: return bound(); // x
+                case 65: return flyToTheLight(); //a
+                case 77: return mouse(); // m
+                case 78: return menu(); // n
+            }
+            return null;
+        });
+        restart();
+    }
+
+    $(document).ready(initialize);
+
+    return {
+        restart : restart,
+        cols : cols,
+        startStop : startStop,
+        bound : bound,
+        mouse : mouse,
+        flyToTheLight : flyToTheLight,
+        nomenu : nomenu
+    };
+})();
